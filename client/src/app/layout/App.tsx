d297a249733f7a417d6fe1c1e68c3dfb51c89997
DaifMoe <85468822+DaifMoe@users.noreply.github.com>
2023-08-19 12:13:47 -0400

    Created Initial Client Side Of The Site

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,34 @@
+import { ThemeProvider } from "@emotion/react";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+import { Container, CssBaseline, createTheme } from "@mui/material";
+import { useState } from "react";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
